d035ab509b2b5998de6b410ccee2df93942a998b
gustavopsan <gustavopsantana4@gmail.com>
2020-03-28 17:33:37 -0300

    Primeiro Commit

---
+++ Javascript/main_chart.js
@@ -0,0 +1,21 @@
+var ctx = $('.main_chart');
+
+var graphic = new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: ['Resíduo Autoconer',
+               'Inicio Rolo Urdideira',
+               'Fio s/ Goma Engomadeira',
+               'Fio c/ Goma Engomadeira',
+               'Fio c/ Goma Tecelagem',
+               'Resíduo Picanol',
+               'Residuo Itema',
+               'Varreduras'
+              ],
+      datasets: [{
+        label: 'Peso (Kg)',
+        data: [10.16, 2.6, 17.52, 25.44, 10.0, 75.2, 14.7, 23.6],
+        backgroundColor: '#394472',
+      }]
+    }
+  });
