125c4aa842a2610769608e7c670c3554e3dab5de
gustavopsan <gustavopsantana4@gmail.com>
2020-03-30 17:34:09 -0300

    Encapsulando dados

--- Javascript/main_chart.js
+++ Javascript/main_chart.js
@@ -1,20 +1,24 @@
+const residues = ['Resíduo Autoconer',
+                'Inicio Rolo Urdideira',
+                'Fio s/ Goma Engomadeira',
+                'Fio c/ Goma Engomadeira',
+                'Fio c/ Goma Tecelagem',
+                'Resíduo Picanol',
+                'Residuo Itema',
+                'Varreduras'
+];
+
+var values = [10.16, 2.6, 17.52, 25.44, 10.0, 75.2, 14.7, 23.6];
+
 var ctx = $('.main_chart');
 
 var graphic = new Chart(ctx, {
-    type: 'bar',
+    type: 'horizontalBar',
     data: {
-      labels: ['Resíduo Autoconer',
-               'Inicio Rolo Urdideira',
-               'Fio s/ Goma Engomadeira',
-               'Fio c/ Goma Engomadeira',
-               'Fio c/ Goma Tecelagem',
-               'Resíduo Picanol',
-               'Residuo Itema',
-               'Varreduras'
-              ],
+      labels: residues,
       datasets: [{
         label: 'Peso (Kg)',
-        data: [10.16, 2.6, 17.52, 25.44, 10.0, 75.2, 14.7, 23.6],
+        data: values,
         backgroundColor: '#394472',
       }]
     }
