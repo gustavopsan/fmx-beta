5bc2f9fc96f1ff0876961aa59bd7f977ae130cee
gustavopsan <gustavopsantana4@gmail.com>
2020-04-06 18:03:37 -0300

    Adding ;Ajax Requests - Receiving data from API

--- Javascript/main_chart.js
+++ Javascript/main_chart.js
@@ -8,7 +8,7 @@ const residues = ['Resíduo Autoconer',
                 'Varreduras'
 ];
 
-var values = [10.16, 2.6, 17.52, 25.44, 10.0, 75.2, 14.7, 23.6];
+var values = [1, 1, 1, 1, 1, 1, 1, 1];
 
 var ctx = $('.main_chart');
 
@@ -22,4 +22,44 @@ var graphic = new Chart(ctx, {
         backgroundColor: '#394472',
       }]
     }
-  });
+});
+
+function updateChart(chart, data) {
+	chart.data.datasets[0].data = data;
+	chart.update();
+	console.log('Chart Updated Sucessfuly');
+};
+
+$('#btn_go').click( function () {
+	
+	var date = $('#date').val();
+
+	$.get( `http://localhost:3000/fm_xxx/${date}`, function( data ) {
+
+        $('#doc_num').val(data.data.id_form)
+        $('#autoconer').html(data.data.autoconer);
+        $('#urdideira').html(data.data.urdideira);
+        $('#fsg_eng').html(data.data.fsg_eng);
+        $('#fcg_eng').html(data.data.fcg_eng);
+        $('#fcg_tec').html(data.data.fcg_tecelagem);
+        $('#picanol').html(data.data.res_picanol);
+        $('#itema').html(data.data.res_itema);
+		$('#varredura').html(data.data.varreduras);
+		
+		var data1 = data.data.autoconer;
+		var data2 = data.data.urdideira;
+		var data3 = data.data.fsg_eng;
+		var data4 = data.data.fcg_eng;
+		var data5 = data.data.fcg_tecelagem;
+		var data6 = data.data.res_picanol;
+		var data7 = data.data.res_itema;
+		var data8 = data.data.varreduras;
+
+		var newData = [data1, data2, data3, data4, data5, data6, data7, data8];
+
+		updateChart(graphic, newData);
+
+    }).fail( function (err) {
+        console.log(err);
+        });
+})
