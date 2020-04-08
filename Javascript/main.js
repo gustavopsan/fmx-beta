2a785984aa48dac84018b7fb8eb36982a574600a
Gustavo P. Santana <58692651+gustavopsan@users.noreply.github.com>
2020-04-08 19:30:31 -0300

    Returning the last commit

--- Javascript/main.js
+++ Javascript/main.js
@@ -1,6 +1,6 @@
 $(document).ready(() => {
     $('#btn_form').click((e) => { 
         e.preventDefault();
-        $(location).attr('href', '../tree/master/Coleta/fmxxx/index.html');
+        $(location).attr('href', '../Coleta/fmxxx/index.html');
     });
 });
