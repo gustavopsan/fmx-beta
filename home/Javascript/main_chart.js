c0f2dae6547d6f52b349f63a464a183219e35363
gustavopsan <gustavopsantana4@gmail.com>
2020-05-02 11:20:08 -0300

    Verifivando existência de formulários na Home

--- home/Javascript/main_chart.js
+++ home/Javascript/main_chart.js
@@ -27,39 +27,44 @@ var graphic = new Chart(ctx, {
 function updateChart(chart, data) {
 	chart.data.datasets[0].data = data;
 	chart.update();
-	console.log('Chart Updated Sucessfuly');
 };
 
 $('#btn_go').click( function () {
 	
 	var date = $('#date').val();
 
-	$.get( `https://api-fmx.herokuapp.com/fm_xxx/${date}`, function( data ) {
+	$.get( `https://api-fmx.herokuapp.com/fm_xxx/${date}`, function(data) {
 
-        $('#doc_num').val(data.data.id_form)
-        $('#autoconer').html(data.data.autoconer);
-        $('#urdideira').html(data.data.urdideira);
-        $('#fsg_eng').html(data.data.fsg_eng);
-        $('#fcg_eng').html(data.data.fcg_eng);
-        $('#fcg_tec').html(data.data.fcg_tecelagem);
-        $('#picanol').html(data.data.res_picanol);
-        $('#itema').html(data.data.res_itema);
-		$('#varredura').html(data.data.varreduras);
-		
-		var data1 = data.data.autoconer;
-		var data2 = data.data.urdideira;
-		var data3 = data.data.fsg_eng;
-		var data4 = data.data.fcg_eng;
-		var data5 = data.data.fcg_tecelagem;
-		var data6 = data.data.res_picanol;
-		var data7 = data.data.res_itema;
-		var data8 = data.data.varreduras;
+    if(data.data === undefined){
+      $('#alerta').html('Formulário não encontrado!');
+    } else {
 
-		var newData = [data1, data2, data3, data4, data5, data6, data7, data8];
+      $('#alerta').html(null);
 
-		updateChart(graphic, newData);
+      $('#doc_num').val(data.data.id_form)
+      $('#autoconer').html(data.data.autoconer);
+      $('#urdideira').html(data.data.urdideira);
+      $('#fsg_eng').html(data.data.fsg_eng);
+      $('#fcg_eng').html(data.data.fcg_eng);
+      $('#fcg_tec').html(data.data.fcg_tecelagem);
+      $('#picanol').html(data.data.res_picanol);
+      $('#itema').html(data.data.res_itema);
+      $('#varredura').html(data.data.varreduras);
+      
+      var data1 = data.data.autoconer;
+      var data2 = data.data.urdideira;
+      var data3 = data.data.fsg_eng;
+      var data4 = data.data.fcg_eng;
+      var data5 = data.data.fcg_tecelagem;
+      var data6 = data.data.res_picanol;
+      var data7 = data.data.res_itema;
+      var data8 = data.data.varreduras;
 
-    }).fail( function (err) {
-        console.log(err);
-        });
-})
+      var newData = [data1, data2, data3, data4, data5, data6, data7, data8];
+
+      updateChart(graphic, newData);
+    }
+    
+
+  })
+});
