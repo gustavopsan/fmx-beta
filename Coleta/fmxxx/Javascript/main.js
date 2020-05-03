a8cf84e1926c50aa4bdd09dde47a53ffbf647c80
gustavopsan <gustavopsantana4@gmail.com>
2020-05-03 13:08:38 -0300

    Checking session

--- Coleta/fmxxx/Javascript/main.js
+++ Coleta/fmxxx/Javascript/main.js
@@ -1,3 +1,18 @@
+function charge () {
+    $.get("https://api-fmx.herokuapp.com/check", function (data) {
+        if (data.status === false) {
+            //window.location.href = '../';
+        } else if (data.status === true) {
+            var username;
+            $.get("https://api-fmx.herokuapp.com/getUser", function (data) {
+                console.log(data);
+            })
+        }
+    });
+}
+
+window.addEventListener('load', charge, false);
+
 $(document).ready(function () {
       
     //Configurando a passagem entre os campos de texto
--- home/Javascript/main.js
+++ home/Javascript/main.js
@@ -1,1 +1,14 @@
+function charge () {
+    $.get("https://api-fmx.herokuapp.com/check", function (data) {
+        if (data.status === false) {
+            //window.location.href = '../';
+        } else if (data.status === true) {
+            var username;
+            $.get("https://api-fmx.herokuapp.com/getUser", function (data) {
+                console.log(data);
+            })
+        }
+    });
+}
 
+window.addEventListener('load', charge, false);
