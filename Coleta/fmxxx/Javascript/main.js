a8a7cb08ef5a482a0c758b4bf489979b856a118c
gustavopsan <gustavopsantana4@gmail.com>
2020-05-03 13:45:43 -0300

    fixing

--- Coleta/fmxxx/Javascript/main.js
+++ Coleta/fmxxx/Javascript/main.js
@@ -1,12 +1,10 @@
 function charge () {
     $.get("https://api-fmx.herokuapp.com/check", function (data) {
         if (data.status === false) {
-            //window.location.href = '../';
-            alert(data.status);
+            window.location.href = '../';
         } else if (data.status === true) {
-            alert(data.status);
             $.get("https://api-fmx.herokuapp.com/getUser", function (data) {
-                $('#user').val(data.username);
+                $('#user').html(data.username);
             })
         }
     });
