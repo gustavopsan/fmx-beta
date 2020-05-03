8005475869ef9dd65a20801fd7418d162d46fbe9
gustavopsan <gustavopsantana4@gmail.com>
2020-05-03 13:41:20 -0300

    serhatgjdbçsionça

--- Coleta/fmxxx/Javascript/main.js
+++ Coleta/fmxxx/Javascript/main.js
@@ -1,9 +1,10 @@
 function charge () {
     $.get("https://api-fmx.herokuapp.com/check", function (data) {
         if (data.status === false) {
-            //window.location.href = '../../';
+            //window.location.href = '../';
             alert(data.status);
         } else if (data.status === true) {
+            alert(data.status);
             $.get("https://api-fmx.herokuapp.com/getUser", function (data) {
                 $('#user').val(data.username);
             })
